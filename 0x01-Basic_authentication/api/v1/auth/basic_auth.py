97a193b33a0fc3bb824eb68ea0da2e1aa731323a
2Kelvin <rocketmancodes02@gmail.com>
2024-02-12 17:42:33 +0300

    retrieve the authorization credentials from base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,12 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic authentication class"""
-    pass
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """Decoding base64 authorization credentials to a string"""
+        if authorization_header is None or type(authorization_header) != str:
+            return None
+        if authorization_header[:6] != 'Basic ':
+            return None
+        return authorization_header[6:]
