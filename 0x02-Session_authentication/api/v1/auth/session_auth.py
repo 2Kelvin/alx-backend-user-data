9943897b55557723562a1aeecbac4dd098a99c90
2Kelvin <rocketmancodes02@gmail.com>
2024-02-14 16:36:00 +0300

    get the user from the cookie value

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """Session Authentication module"""
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -25,3 +26,12 @@ class SessionAuth(Auth):
         if session_id is None or type(session_id) != str:
             return None
         return SessionAuth.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> User:
+        """Returns a User based on the cookie value"""
+        # getting the cookie value
+        cookieValue = self.session_cookie(request)
+        # using the cookie value as sessionID to fetch user_id
+        userID = self.user_id_for_session_id(cookieValue)
+        # fetching the user (obj)
+        return User.get(userID)
