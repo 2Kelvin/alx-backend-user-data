23e75e7bbb9bee79e0b710204cb1287b23010d5c
2Kelvin <rocketmancodes02@gmail.com>
2024-02-14 17:32:32 +0300

    add login route

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Session Routes Module"""
+
+from typing import Tuple
+from api.v1.views import app_views
+from flask import request, jsonify
+from models.user import User
+from os import getenv
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def authenticateSessions() -> Tuple[str, int]:
+    """Handles all routes for session authentication"""
+    usrEmail = request.form.get('email')
+    usrPassword = request.form.get('password')
+    if usrEmail is None or len(usrEmail.strip()) == 0:
+        return jsonify({"error": "email missing"}), 400
+    if usrPassword is None or len(usrPassword.strip()) == 0:
+        return jsonify({"error": "password missing"}), 400
+    try:
+        users = User.search({'email': usrEmail})
+    except Exception:
+        return jsonify({"error": "no user found for this email"}), 404
+    if len(users) <= 0:
+        return jsonify({"error": "no user found for this email"}), 404
+    if users[0].is_valid_password(usrPassword):
+        from api.v1.app import auth
+        sessionID = auth.create_session(getattr(users[0], 'id'))
+        data = jsonify(users[0].to_json())
+        data.set_cookie(getenv('SESSION_NAME'), sessionID)
+        return data
+    return jsonify({"error": "wrong password"}), 401
