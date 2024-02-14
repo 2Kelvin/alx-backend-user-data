d6e0511473495d67ae7201defea9d701b9980e56
2Kelvin <rocketmancodes02@gmail.com>
2024-02-14 12:12:14 +0300

    add a session auth class

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -19,6 +20,8 @@ authType = getenv('AUTH_TYPE')
 
 if authType == 'basic_auth':
     auth = BasicAuth()
+elif authType == 'session_auth':
+    auth = SessionAuth()
 else:
     auth = Auth()
 
